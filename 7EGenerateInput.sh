0b2470ea42168cf00b63a7a2a69f0bdc20fcfec2
Egor Morunov <e_morunov@wargaming.net>
2022-12-21 10:58:15 +0100

    7E: add input diamond generator

---
+++ 7EGenerateInput.sh
@@ -0,0 +1,1 @@
+testname=7Etest4; j=50001; echo "$((j-1)) $j $j" > $testname; for((i=0;i<j/2;i++)); do echo "$((j/2-i)) $((i+1)) $((j-i-1))" >> $testname; echo "$((j/2+i+1)) $((i+1)) $((j-i-1))" >> $testname; done
