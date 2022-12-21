55c1853ec3556722b6fa46e9aec37074aad6c33b
Egor Morunov <e_morunov@wargaming.net>
2022-12-21 01:40:28 +0100

    7E: event driven implementation, slow on set insert/erase with a lot of elements

--- 7E.cpp
+++ 7E.cpp
@@ -1,57 +1,123 @@
 #include "optimization.h"
 
+#include <algorithm>
+#include <chrono>
+#include <iostream>
 #include <set>
 #include <vector>
 
-int main() {
-    const auto n = readInt<uint32_t>();
-    const auto Y = readInt<uint32_t>();
-    const auto X = readInt<uint32_t>();
-    uint32_t point;
-    uint32_t indexRaceStart;
-    uint32_t indexRaceEnd;
+int nCalcs{0};
 
-    // fill stations
-    std::set<uint32_t>* stations = new std::set<uint32_t>[X + 1];
-    for (uint32_t i = 0; i < n; ++i) {
-        point = readInt<uint32_t>();
-        indexRaceStart = readInt<uint32_t>();
-        indexRaceEnd = readInt<uint32_t>();
-        stations[point].insert(indexRaceStart);
-        stations[point].insert(indexRaceEnd);
+struct Event {
+    int type{0};  // 0 - begin of station, 1 - end of station
+    int x{0};
+    int y{0};
+
+    bool operator<(const Event& other) {
+        if (y != other.y) {
+            return y < other.y;
+        }
+        if (type != other.type) {
+            return type < other.type;
+        }
+        return x < other.x;
+    }
+};
+
+void calculateAndOutput(const std::set<int>& stops, const int nRaces) {
+    if (nRaces <= 0) {
+        return;
+    }
+    ++nCalcs;
+    int distance{0};
+    int prevStop{0};
+    for (const auto stop : stops) {
+        const auto tmpDistance = stop - prevStop;
+        if (tmpDistance > distance) {
+            distance = tmpDistance;
+        }
+        prevStop = stop;
+    }
+    for (int i = 0; i < nRaces; ++i) {
+        writeInt(distance, '\n');
+    }
+}
+
+int main() {
+    const auto t1 = std::chrono::system_clock::now();
+
+    const auto n = readInt<int>();
+    const auto Y = readInt<int>();
+    const auto X = readInt<int>();
+
+    // fill station events
+    std::vector<Event> events;
+    events.reserve(n * 2);
+    for (int i = 0; i < n; ++i) {
+        const auto x = readInt<int>();
+        const auto y1 = readInt<int>();
+        const auto y2 = readInt<int>();
+        events.push_back({0, x, y1});
+        events.push_back({1, x, y2});
     }
 
-    // scan race stops and calculate distance
-    std::vector<uint32_t> distances(Y + 1);
-    std::vector<uint32_t> last_stops(Y + 1);
-    for (uint32_t i = 0; i < X + 1; ++i) {
-        if (stations[i].empty()) {
-            continue;
-        }
-        for (auto it = stations[i].begin(); it != stations[i].end(); ++it) {
-            const auto iRaceBegin = *it;
-            const auto iRaceEnd = *(++it);
-            for (auto j = iRaceBegin; j <= iRaceEnd; ++j) {
-                const auto tmpDistance = i - last_stops[j];
-                if (tmpDistance > distances[j]) {
-                    distances[j] = tmpDistance;
+    const auto t2 = std::chrono::system_clock::now();
+
+    std::sort(events.begin(), events.end());
+
+    const auto t3 = std::chrono::system_clock::now();
+
+    int yPrev = 0;
+    int typePrev = 0;
+    std::set<int> stops{X};  // current state of stops on race (initialize with the last stop on finish)
+    // scan events
+    for (const auto& event : events) {
+        // writeInt(event.y, ' ');
+        // writeInt(event.x, ' ');
+        // writeInt(event.type, '\n');
+        if (event.y != yPrev) {
+            if (typePrev == 0) {
+                if (event.type == 1) {
+                    // writeWord("recalculate and output distances on [yPrev; event.y]\n");
+                    calculateAndOutput(stops, event.y - yPrev + 1);
+                } else {
+                    // writeWord("recalculate and output distances on [yPrev; event.y)\n");
+                    calculateAndOutput(stops, event.y - yPrev);
                 }
-                last_stops[j] = i;
+            } else {
+                if (event.type == 1) {
+                    // writeWord("recalculate and output distances on (yPrev; event.y]\n");
+                    calculateAndOutput(stops, event.y - yPrev);
+                } else {
+                    // writeWord("recalculate and output distances on (yPrev; event.y)\n");
+                    calculateAndOutput(stops, event.y - yPrev - 1);
+                }
+            }
+        } else {
+            if (typePrev == 0 && event.type == 1) {
+                // writeWord("recalculate and output distances on [event.y]\n");
+                calculateAndOutput(stops, 1);
             }
         }
-    }
-
-    // finalize races
-    for (uint32_t i = 0; i < Y + 1; ++i) {
-        const auto tmpDistance = X - last_stops[i];
-        if (tmpDistance > distances[i]) {
-            writeInt(tmpDistance, '\n');
+        if (event.type == 0) {
+            stops.insert(event.x);
         } else {
-            writeInt(distances[i], '\n');
+            stops.erase(event.x);
         }
+        typePrev = event.type;
+        yPrev = event.y;
     }
 
-    delete[] stations;
+    // if some races without station remains at the end
+    calculateAndOutput(stops, Y - yPrev);
+
+    const auto t4 = std::chrono::system_clock::now();
+
+    std::cerr << "Read: " << (t2 - t1).count() / 1000 << "ms\n";
+    std::cerr << "Sort: " << (t3 - t2).count() / 1000 << "ms\n";
+    std::cerr << "Calc and output: " << (t4 - t3).count() / 1000 << "ms\n";
+    std::cerr << "Total: " << (t4 - t1).count() / 1000 << "ms\n";
+    std::cerr << "Calculations count: " << nCalcs << '\n';
 
     return 0;
 }
